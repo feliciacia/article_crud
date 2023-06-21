9977f49b7af78f99eecf060b1ca64f2613366f33
羅翡幸 <feliciaalo08@gmail.com>
2023-06-21 18:08:43 +0800

    get_header_token_value

---
+++ middleware/auth.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No authorization header")})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
